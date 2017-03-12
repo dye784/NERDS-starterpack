446a1b8192e3d4cb6612dd87f2514d0eeab31a34
dye784 <damon.ye91@gmail.com>
2017-03-12 15:15:02 -0400

    finished initial react set up

---
+++ browser/react/App/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default App = () => {
+  return (
+    <div></div>
+  )
+}
---
+++ browser/react/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from'react-redux'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import App from './App';
+
+const Routes = ({ fetchInitialData }) => (
+  <Router history={ browserHistory }>
+    <Route path="/">
+      <IndexRoute component={App} />
+      <Route path="*" component={App} />
+    </Route>
+  </Router>
+);
+
+const mapStateToProps = null;
+
+const mapDispatchToProps = null;
+
+export default connect(mapStateToProps, mapDispatchToProps)(Routes);
