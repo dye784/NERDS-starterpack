6507e6e9b07baf3f66862a76448f93a610888f5d
dye784 <damon.ye91@gmail.com>
2017-06-23 21:08:56 -0400

    fix: invoke json method on auth

--- server/model/_db.js
+++ server/model/_db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/NAME_OF_DB', {
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/chatdb', {
   logging: false,
 });
 
