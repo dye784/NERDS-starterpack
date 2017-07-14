04782cc8f558f119348f1654ad95245bb2e5837a
dye784 <damon.ye91@gmail.com>
2017-07-14 09:15:40 -0400

    feat: display warning message when passwords do not match

--- browser/SignUp/index.jsx
+++ browser/SignUp/index.jsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createNewUser } from '../Login/LoginActionCreator';
 
-export const SignUpForm = ({ createNewUser }) => {
-  const handleSubmit = (evt) => {
+export class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      warning: false,
+    };
+  }
+
+  handleSubmit = (evt) => {
     evt.preventDefault();
     if (evt.target.password.value === evt.target.confirm.value) {
-      createNewUser(evt.target.username.value, evt.target.password.value);
+      this.props.createNewUser(evt.target.username.value, evt.target.password.value);
+      this.setState({ warning: false });
+    } else {
+      this.setState({ warning: true });
     }
   };
 
-  return (
-    <div>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <input name="username" placeholder="username" />
-        <input name="password" type="password" placeholder="password" />
-        <input name="confirm" type="password" placeholder="confirm password" />
-        <input type="submit" value="Sign Up" />
-      </form>
-    </div>
-  );
+  render () {
+    return (
+      <div>
+        <h1>Sign Up</h1>
+        {this.state.warning && <h5>The passwords do not match</h5>}
+        <form onSubmit={this.handleSubmit}>
+          <input name="username" placeholder="username" />
+          <input name="password" type="password" placeholder="password" />
+          <input name="confirm" type="password" placeholder="confirm password" />
+          <input type="submit" value="Sign Up" />
+        </form>
+      </div>
+    );
+  }
 };
 
 const mapDispatchToProps = { createNewUser };
