86b487b67aa02ae58a163dd274f8b75dde6dec16
dye784 <damon.ye91@gmail.com>
2017-07-14 00:05:21 -0400

    feat: add signup form

---
+++ browser/SignUp/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createNewUser } from '../Login/LoginActionCreator';
+
+export const SignUpForm = ({ createNewUser }) => {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (evt.target.password.value === evt.target.confirm.value) {
+      createNewUser(evt.target.username.value, evt.target.password.value);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input name="username" placeholder="username" />
+        <input name="password" type="password" placeholder="password" />
+        <input name="confirm" type="confirm" placeholder="confirm password" />
+        <input type="submit" value="Sign Up" />
+      </form>
+    </div>
+  );
+};
+
+const mapDispatchToProps = { createNewUser };
+
+export default connect(null, mapDispatchToProps)(SignUpForm);
