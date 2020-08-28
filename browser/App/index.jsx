97636e9754c0f961d6d22b8bb0b169c0e05b3a6d
Damon Ye <dye@grubhub.com>
2020-08-28 16:55:29 -0400

    Feat: fetch loggedin user to front end to make sessions work

--- browser/App/index.jsx
+++ browser/App/index.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Login from '../Login';
 import Logout from '../Logout';
 import { getUser } from '../Login/LoginReducer';
 import SignUpForm from '../SignUp';
 import './style.scss';
+import { fetchLoggedInUser } from '../Login/LoginActionCreator';
 
-export const App = (props) => {
-  return (
-    <div>
-      <h1>HELLO WORLD</h1>
-      {!props.user ? <Login /> : <Logout />}
-      {!props.user && <SignUpForm />}
-    </div>
-  );
+export class App extends Component {
+    componentDidMount() {
+        this.props.fetchLoggedInUser();
+    }
+
+    render() {
+        const { user } = this.props;
+        return (
+            <div>
+                <h1>HELLO WORLD</h1>
+                {!user ? <Login /> : <Logout />}
+                {!user && <SignUpForm />}
+            </div>
+        );
+    }
 };
 
 const mapStateToProps = (state) => ({ user: getUser(state) });
+const mapDispatchToProps = (dispatch) => ({ fetchLoggedInUser: () => dispatch(fetchLoggedInUser()) })
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
